177d2e05455219ee76501340fb6f1f54419d8843
Gaurav <gauravsomani52750@gmail.com>
2023-05-14 14:05:43 +0530

    bug fixed 2

--- hotel/settings.py
+++ hotel/settings.py
@@ -78,7 +78,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://localhost:3000',
     'http://127.0.0.1:8001',
-    'http://127.0.0.1:3000',
+    # 'http://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'hotel.urls'
